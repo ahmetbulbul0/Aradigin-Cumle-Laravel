858f6af88059ceac3c0ce5ee4a210eb89d76e1a0
Ahmet Bülbül <ahmetbulbul@protonmail.com>
2022-01-31 16:08:04 +0300

    news list controllerıları route tanımlamaları yapıldı

--- app/Http/Controllers/Tools/CategoryGroupToText.php
+++ app/Http/Controllers/Tools/CategoryGroupToText.php
@@ -7,12 +7,21 @@ use App\Models\CategoryGroupsModel;
 
 class CategoryGroupToText extends Controller
 {
+    static function multiple($data) {
+        $count = count($data["data"]);
+        for ($i = 0; $i < $count; $i++) {
+            $item = $data["data"][$i]["category"]["no"];
+            $data["data"][$i]["category"]["text"] = CategoryGroupToText::single($item);
+        }
+        return $data;
+    }
+
     static function single($no) {
         $get = CategoryGroupsModel::where("no", $no)->with("main", "sub1", "sub2", "sub3", "sub4", "sub5")->first()->toArray();
         $data = [
             "main" => $get["main"]["name"],
-            "sub1" => $get["sub1"]["name"] ?? "deneme1",
-            "sub2" => $get["sub2"]["name"] ?? "denem4",
+            "sub1" => $get["sub1"]["name"] ?? NULL,
+            "sub2" => $get["sub2"]["name"] ?? NULL,
             "sub3" => $get["sub3"]["name"] ?? NULL,
             "sub4" => $get["sub4"]["name"] ?? NULL,
             "sub5" => $get["sub5"]["name"] ?? NULL
---
+++ resources/views/system/components/news_list.blade.php
@@ -0,0 +1,96 @@
+<div class="outDbList">
+    <div class="inDbList">
+        <div class="outTitle">
+            <div class="inTitle">
+                Kategoriler
+            </div>
+            <div class="titleSelects">
+                <div class="listingTypeSelect">
+                    <form method="POST" class="outSelectBox">
+                        @csrf
+                        <select name="listingType" onchange="if(this.value != 0) { this.form.submit(); }">
+                            <option value="default" @if (Route::is('haberler')) selected @endif>Varsayılan</option>
+                            <option value="no09" @if (Route::is('haberler_no09')) selected @endif>No (0 - 9)</option>
+                            <option value="no90" @if (Route::is('haberler_no90')) selected @endif>No (9 - 0)</option>
+                            <option value="contentAZ" @if (Route::is('haberler_contentAZ')) selected @endif>İçerik (A - Z)</option>
+                            <option value="contentZA" @if (Route::is('haberler_contentZA')) selected @endif>İçerik (Z - A)</option>
+                            <option value="authorAZ" @if (Route::is('haberler_authorAZ')) selected @endif>Yazar (A - Z)</option>
+                            <option value="authorZA" @if (Route::is('haberler_authorZA')) selected @endif>Yazar (Z - A)</option>
+                            <option value="categoryAZ" @if (Route::is('haberler_categoryAZ')) selected @endif>Kategori (A - Z)</option>
+                            <option value="categoryZA" @if (Route::is('haberler_categoryZA')) selected @endif>Kategori (Z - A)</option>
+                            <option value="resourcePlatformAZ" @if (Route::is('haberler_resourcePlatformAZ')) selected @endif>Kaynak Site (A - Z)</option>
+                            <option value="resourcePlatformZA" @if (Route::is('haberler_resourcePlatformZA')) selected @endif>Kaynak Site (Z - A)</option>
+                            <option value="publishDateAZ" @if (Route::is('haberler_publishDateAZ')) selected @endif>Yayın Zamanı (A - Z)</option>
+                            <option value="publishDateZA" @if (Route::is('haberler_publishDateZA')) selected @endif>Yayın Zamanı (Z - A)</option>
+                            <option value="writeTimeAZ" @if (Route::is('haberler_writeTimeAZ')) selected @endif>Yazılma Zamanı (A - Z)</option>
+                            <option value="writeTimeZA" @if (Route::is('haberler_writeTimeZA')) selected @endif>Yazılma Zamanı (Z - A)</option>
+                        </select>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="dbList">
+            <div class="titleLine">
+                <div class="w5">
+                    <span>No</span>
+                </div>
+                <div class="w10">
+                    <span>Yazar</span>
+                </div>
+                <div class="w10">
+                    <span>Kategori</span>
+                </div>
+                <div class="w30">
+                    <span>İçerik</span>
+                </div>
+                <div class="w10">
+                    <span>Kaynak Site</span>
+                </div>
+                <div class="w15">
+                    <span>Yayın Zamanı</span>
+                </div>
+                <div class="w15">
+                    <span>Yazılma Zamanı</span>
+                </div>
+                <div class="w5">
+                    <span>İşlem</span>
+                </div>
+            </div>
+            @foreach ($data['data'] as $item)
+                <div class="line">
+                    <div class="w5">
+                        <span>{{ $item['no'] }}</span>
+                    </div>
+                    <div class="w10">
+                        <span>{{ $item['author']["username"] }}</span>
+                    </div>
+                    <div class="w10">
+                        <span>{{ $item['category']["text"] }}</span>
+                    </div>
+                    <div class="w30">
+                        <span>{{ $item['content'] }}</span>
+                    </div>
+                    <div class="w10">
+                        <span>{{ $item['resource_platform']["name"] }}</span>
+                    </div>
+                    <div class="w15">
+                        <span>{{ $item['publish_date']["text"] }}</span>
+                    </div>
+                    <div class="w15">
+                        <span>{{ $item['write_time']["text"] }}</span>
+                    </div>
+                    <div class="actions w5">
+                        <span>
+                            <a href="/sistem-paneli/kategori-tipi/{{ $item['no'] }}/düzenle">
+                                <i class="fas fa-edit"></i>
+                            </a>
+                            <a href="/sistem-paneli/kategori-tipi/{{ $item['no'] }}/sil">
+                                <i class="fas fa-trash"></i>
+                            </a>
+                        </span>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</div>
