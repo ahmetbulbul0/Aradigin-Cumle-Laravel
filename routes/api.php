1fc0f96b9741e6346625bfdc7d45f74506c2a1d7
Ahmet Bülbül <ahmetbulbul@protonmail.com>
2022-02-05 16:00:12 +0300

    clearing.1

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,3 @@
 use App\Http\Controllers\Api\Categories\CategoryCreateController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::post("/category-create", [CategoryCreateController::class, "get"]);
