459bdccf084363c34c5229148588c127577099fe
Ahmet Bülbül <87360838+ahmetbulbul0@users.noreply.github.com>
2022-03-21 20:38:07 +0300

    this directory is now static

--- public/src/js/welcome_how_are_you.js
+++
@@ -1,8 +0,0 @@
-function welcomeHowAreYouHaveDifferentThink() {
-    document.getElementById("defaultOptions").style.display = "none";
-    document.getElementById("haveDifferentThink").style.display = "block";
-}
-function welcomeHowAreYouHaveDefaultOption() {
-    document.getElementById("defaultOptions").style.display = "block";
-    document.getElementById("haveDifferentThink").style.display = "none";
-}
