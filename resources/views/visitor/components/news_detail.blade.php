43fbbb0bb77d922748935edf73c3bf0eba062f4f
Ahmet Bülbül <ahmetbulbul@protonmail.com>
2022-03-28 22:09:18 +0300

    news detail some changes

--- resources/views/visitor/components/news_detail.blade.php
+++ resources/views/visitor/components/news_detail.blade.php
@@ -4,11 +4,11 @@
             <div class="outNewsDetail">
                 <div class="inNewsDetail">
                     <div class="line">
-                        <span class="date">{{ $data['newsDetail']['data']['publish_date']['text'] }}</span>
-                        <span class="writer">Yazar: <a href="{{ route('haberler_listesi_yazar', [$data['newsDetail']['data']['author']['username'], 'son-yayinlananlar']) }}">{{ Str::title($data['newsDetail']['data']['author']['full_name']) }}</a></span>
+                        <span class="content">{{ $data['newsDetail']['data']['content'] }}</span>
                     </div>
                     <div class="line">
-                        <span class="content">{{ $data['newsDetail']['data']['content'] }}</span>
+                        <span class="writer">Yazar: <a href="{{ route('haberler_listesi_yazar', [$data['newsDetail']['data']['author']['username'], 'son-yayinlananlar']) }}">{{ $data['newsDetail']['data']['author']['username'] }} [{{ Str::title($data['newsDetail']['data']['author']['full_name']) }}]</a></span>
+                        <span class="date">{{ $data['newsDetail']['data']['publish_date']['text'] }}</span>
                     </div>
                     <div class="line category">
                         <span class="category">
