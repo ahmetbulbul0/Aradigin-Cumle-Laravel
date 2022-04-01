0494f10a97fd6a8440840ef6ad7c0aba9f8c4e4a
Ahmet Bülbül <ahmetbulbul@protonmail.com>
2022-04-02 00:18:44 +0300

    pwa removed

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -9,7 +9,6 @@
     <title>Aradığın Cümle | @yield('page_title')</title>
     <link rel="stylesheet" href="{{ URL::asset('src/css/style.css') }}">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
-    @laravelPWA
 </head>
 
 <body class="dark">
