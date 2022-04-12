5fce0373e355bf2d5c6782eb124ad71552b2d79d
Ahmet Bülbül <ahmetbulbul@protonmail.com>
2022-04-12 03:21:31 +0300

    visitor style minifed

--- resources/views/visitor/layout.blade.php
+++ resources/views/visitor/layout.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aradığın Cümle | {{ $data['page_title'] ?? 'TANIMLANMAMIŞ SAYFA' }}</title>
-    <link rel="stylesheet" type="text/css" href="{{ URL::asset('src/css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('src/css/style.min.css') }}">
     <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"
         integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
 </head>
