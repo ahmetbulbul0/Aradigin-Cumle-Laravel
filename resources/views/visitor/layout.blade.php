ef1dfdd9a96fbd1f07defa29b79ac400d39ddac5
Ahmet Bülbül <ahmetbulbul@protonmail.com>
2022-04-12 03:00:08 +0300

    sign in page fixed and visitor layot edited now form submit refresh page error fixed

--- resources/views/visitor/layout.blade.php
+++ resources/views/visitor/layout.blade.php
@@ -6,18 +6,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aradığın Cümle | {{ $data["page_title"] ?? "TANIMLANMAMIŞ SAYFA" }}</title>
+    <title>Aradığın Cümle | {{ $data['page_title'] ?? 'TANIMLANMAMIŞ SAYFA' }}</title>
     <link rel="stylesheet" type="text/css" href="{{ URL::asset('src/css/style.css') }}">
-    <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"
+        integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
 </head>
 
-<body class="{{ Session::get("visitorData.website_theme") ?? Session::get("userData.settings.website_theme")  }}">
+<body class="{{ Session::get('visitorData.website_theme') ?? Session::get('userData.settings.website_theme') }}">
     <div class="pageContainer">
         <div class="contentWrap">
             @yield('content')
         </div>
-        @include("visitor.components.footer")
+        @include('visitor.components.footer')
     </div>
 </body>
+
+<script>
+    if (window.history.replaceState) {
+        window.history.replaceState(null, null, window.location.href);
+    }
+</script>
 
 </html>
